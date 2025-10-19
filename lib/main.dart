252472dfc360e6840c8b542f8be4a2c419747b87
Thwin Htoo Aung <htoohtoo.mdy.mmr@gmail.com>
2025-10-19 18:17:15 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:real_quest/home_screen.dart';
+
+void main() {
+  runApp(const RealQuest());
+}
+
+class RealQuest extends StatelessWidget {
+  const RealQuest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: const Color(0xFFEEEEEE),
+        scaffoldBackgroundColor: const Color(0xFFEEEEEE),
+        appBarTheme: const AppBarTheme(backgroundColor: Color(0xFFEEEEEE)),
+        textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white)),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
